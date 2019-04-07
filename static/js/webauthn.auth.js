a6f075d2ae7983aac269a19cb100b41332810731
nerocrux <nerocrux@gmail.com>
2019-04-07 22:57:48 +0900

    registration

--- static/js/webauthn.auth.js
+++ static/js/webauthn.auth.js
@@ -1,2 +1,57 @@
 'use strict';
 
+$('#register').submit(function(event) {
+    event.preventDefault();
+    let username = this.username.value;
+    let name = this.name.value;
+    if (!username || !name) {
+        alert("Name or username is missing!")
+        return
+    }
+
+	getMakeCredentialsChallenge({username, name})
+	.then((response) => {
+        let publicKey = preformatMakeCredReq(response)
+        return navigator.credentials.create({publicKey})
+	})
+    .then((newCred) => {
+        let makeCredResponse = publicKeyCredentialToJSON(newCred);
+        console.log(makeCredResponse)
+    })
+})
+
+let getMakeCredentialsChallenge = (formBody) => {
+    return fetch('/webauthn/register', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formBody)
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        if(response.status !== 'ok')
+            throw new Error(`Server responed with error. The message is: ${response.message}`);
+
+        return response
+    })
+}
+
+let sendWebAuthnResponse = (body) => {
+    return fetch('/webauthn/response', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        if(response.status !== 'ok')
+            throw new Error(`Server responed with error. The message is: ${response.message}`);
+
+        return response
+    })
+}
